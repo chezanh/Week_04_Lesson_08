57daf33edee7595c12b2199ce7db39408b592bed
chezanh <thuyanh@thynh.net>
2025-03-25 11:05:52 +0100

    implemented the dropdown on the about

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,3 +6,21 @@ document
     content.style.display =
       content.style.display === "block" ? "none" : "block";
   });
+
+document
+  .querySelector(".dropdown-skills")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const content = document.querySelector(".dropdown-skills-content");
+    content.style.display =
+      content.style.display === "block" ? "none" : "block";
+  });
+
+document
+  .querySelector(".dropdown-experience")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const content = document.querySelector(".dropdown-experience-content");
+    content.style.display =
+      content.style.display === "block" ? "none" : "block";
+  });
